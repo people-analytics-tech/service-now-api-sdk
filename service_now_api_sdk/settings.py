7d2bf9a2582956e18cc293fb0710496ca31240f1
Guilherme Laércio da Silva <manager.guilherme.silva@gmail.com>
2024-09-04 14:42:32 -0300

    Update settings.py

--- service_now_api_sdk/settings.py
+++ service_now_api_sdk/settings.py
@@ -1,6 +1,6 @@
 import os
 
-SERVICENOW_URL = os.environ["SERVICENOW_URL"]
+SERVICENOW_URL = os.environ.get("SERVICENOW_URL")
 SERVICENOW_API_TOKEN = os.environ.get("SERVICENOW_API_TOKEN")
 
 SERVICENOW_API_USER = os.environ.get("SERVICENOW_API_USER")
